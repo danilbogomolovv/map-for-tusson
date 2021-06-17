4a14431de27ff2264c080544adce68682426e1ef
danilbogomolov <danilbogomolov.1710@gmail.com>
2021-06-17 19:02:47 +0300

    add model for Terminals and xml parser

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Terminal(models.Model):
+	cimei = models.CharField(max_length = 50, null = True)
+	inr = models.CharField(max_length = 50, null = True)
+	ctid = models.CharField(max_length = 50, null = True)
+	cmid = models.CharField(max_length = 50, null = True)
+	cpodr = models.CharField(max_length = 50, null = True)
+	cadres = models.CharField(max_length = 50, null = True)
+	cgorod = models.CharField(max_length = 50, null = True)
+	cobl = models.CharField(max_length = 50, null = True)
+	craion = models.CharField(max_length = 50, null = True)
+	ddatan = models.DateField(max_length = 50, null = True)
+	cname = models.CharField(max_length = 70, null = True)
+
+
+
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from lxml import etree
+import xml.etree.ElementTree as ET
+from .models import Terminal
 
 def index(request):
     context = {}
 
+    if len(Terminal.objects.all()) == 0: 
+        parser = ET.XMLParser(encoding="windows-1251")
+        tree = ET.parse("terminals.xml", parser=parser)
+        root = tree.getroot()
+        for child in root:
+            new_terminal = Terminal(cimei = child[0].text, inr = child[1].text, ctid = child[2].text, cmid = child[3].text, cpodr = child[4].text,
+                                    cadres = child[5].text, cgorod = child[6].text, cobl = child[7].text, craion = child[8].text,
+                                    ddatan = child[9].text, cname = child[10].text)
+            new_terminal.save()
+            #print(child[0].text)
+            #print(child[1].text)
+            #print(child[2].text)
+            #print(child[3].text)
+            #print(child[4].text)
+            #print(child[5].text)
+            #print(child[6].text)
+            #print(child[7].text)
+            #print(child[8].text)
+            #print(child[9].text)
+            #print(child[10].text)
+            #for info in child:
+            #    print(info.text)
+            #print('----------------------------------------------------------------------')
+            #print(child.tag, child.attrib)
+    else:
+        print(len(Terminal.objects.all()))
 
+    context['terminals'] = Terminal.objects.all()
     return render(request, 'mainpage/mainpage.html', context)
