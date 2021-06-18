030428403761e84a3657fc3907907a3fdc0b908d
danilbogomolov <danilbogomolov.1710@gmail.com>
2021-06-18 20:30:44 +0300

    add clustering

--- mainpage/views.py
+++ mainpage/views.py
@@ -30,8 +30,32 @@ def index(request):
                                         ddatan = child[9].text, cname = child[10].text)  
                 new_error_terminal.save() 
                 print('ERROR ' + str(count))
-                count = count + 1      
+                count = count + 1 
 
+#    count = 0
+ 
+    for i in ErrorTerminal.objects.all():   
+        print(i.cadres)
+    print('ERROR ' + str(len(ErrorTerminal.objects.all())))  
+    print('OK ' + str(len(Terminal.objects.all()))) 
+#        try:
+#        gmaps = googlemaps.Client(key='AIzaSyC_CpD9oSCYYDu92Jq8EiIGklCgyelDbiw')
+#        geocode_result = gmaps.geocode(i.cadres)
+#        if geocode_result != []:
+#            new_terminal = Terminal(cimei = i.cimei, inr = i.inr, ctid = i.ctid, cmid = i.cmid, cpodr = i.cpodr,
+#                                    cadres = i.cadres, cgorod = i.cgorod, cobl = i.cobl, craion = i.craion,
+#                                    ddatan = i.ddatan, cname = i.cname, lat = geocode_result[0]['geometry']['location']['lat'],
+#                                    lng = geocode_result[0]['geometry']['location']['lng'])
+#            new_terminal.save()
+#            print('OK ' + str(count))
+#            count = count + 1
+#            i.delete()
+#        except Exception as e:
+#        else:
+#            print('ERROR ' + str(count))
+#            count = count + 1
+#    print('ERROR ' + str(len(ErrorTerminal.objects.all())))  
+#    print('OK ' + str(len(Terminal.objects.all())))
 
     context['terminals'] = Terminal.objects.all()
     return render(request, 'mainpage/mainpage.html', context)
