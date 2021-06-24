53acae532180b8667f075fe3b13ef4af4ecf6059
danilbogomolov <danilbogomolov.1710@gmail.com>
2021-06-24 16:15:39 +0300

    delete wrong commit2

--- tusson/settings.py
+++ tusson/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-*y)6x(1#nylmzoqj^4c(q34imzx$&tywusfvkkn=jfqci$s17y'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-*y)6x(1#nylmzoqj^4c(q34imzx$&tywusfvkkn=jfqci$s17y')
+SECRET_KEY = 'django-insecure-*y)6x(1#nylmzoqj^4c(q34imzx$&tywusfvkkn=jfqci$s17y'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -132,8 +132,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
