d1f852cbbccdee894b7d8b2ede69caa9b4ee4074
danilbogomolov <danilbogomolov.1710@gmail.com>
2021-06-26 17:26:56 +0300

    added partner search by input field

--- mainpage/views.py
+++ mainpage/views.py
@@ -57,9 +57,6 @@ def index(request):
 
     if len(Terminal.objects.all()) == 0: 
         count = 0
-        count_zone = 0
-        lats = []
-        lngs = []
         parser = ET.XMLParser(encoding="windows-1251")
         tree = ET.parse("terminals5.xml", parser=parser)
         root = tree.getroot()
@@ -73,30 +70,18 @@ def index(request):
                 gmaps = googlemaps.Client(key='AIzaSyC_CpD9oSCYYDu92Jq8EiIGklCgyelDbiw')
                 geocode_result = gmaps.geocode(child[8].text)
 
+                new_terminal = Terminal(cimei = child[0].text, inr = child[1].text, ctid = child[2].text, cmid = child[3].text, cpodr = child[4].text,
+                                        cobl = child[5].text, craion = child[6].text, cgorod = child[7].text, cadres = child[8].text,
+                                        ddatan = child[9].text, cname = child[10].text, cparta = child[11].text, cots = child[12].text,
+                                        czona = child[13].text, zona_name = zona_name,
+                                        lat = geocode_result[0]['geometry']['location']['lat'],
+                                        lng = geocode_result[0]['geometry']['location']['lng'])
+                new_terminal.save()
+                lats.append(geocode_result[0]['geometry']['location']['lat'])
+                lngs.append(geocode_result[0]['geometry']['location']['lng'])
 
-                if geocode_result[0]['geometry']['location']['lat'] not in lats and geocode_result[0]['geometry']['location']['lng'] not in lngs:
-                    new_terminal = Terminal(cimei = child[0].text, inr = child[1].text, ctid = child[2].text, cmid = child[3].text, cpodr = child[4].text,
-                                            cobl = child[5].text, craion = child[6].text, cgorod = child[7].text, cadres = child[8].text,
-                                            ddatan = child[9].text, cname = child[10].text, cparta = child[11].text, cots = child[12].text,
-                                            czona = child[13].text, zona_name = zona_name, exist = 'false',
-                                            lat = geocode_result[0]['geometry']['location']['lat'],
-                                            lng = geocode_result[0]['geometry']['location']['lng'])
-                    new_terminal.save()
-                    lats.append(geocode_result[0]['geometry']['location']['lat'])
-                    lngs.append(geocode_result[0]['geometry']['location']['lng'])
-
-                    print('OK ' + str(count) + str(child[8].text))
-                    count = count + 1
-                else:
-                    new_terminal = Terminal(cimei = child[0].text, inr = child[1].text, ctid = child[2].text, cmid = child[3].text, cpodr = child[4].text,
-                                            cobl = child[5].text, craion = child[6].text, cgorod = child[7].text, cadres = child[8].text,
-                                            ddatan = child[9].text, cname = child[10].text, cparta = child[11].text, cots = child[12].text,
-                                            czona = child[13].text, zona_name = zona_name, exist = 'true',
-                                            lat = geocode_result[0]['geometry']['location']['lat'],
-                                            lng = geocode_result[0]['geometry']['location']['lng'])
-                    new_terminal.save()   
-                    print('EXIST ' + str(count) + str(child[8].text))
-                    count = count + 1 
+                print('OK ' + str(count) + str(child[8].text))
+                count = count + 1
             except Exception as e:
                 new_error_terminal = ErrorTerminal(cimei = child[0].text, inr = child[1].text, ctid = child[2].text, cmid = child[3].text, cpodr = child[4].text,
                                         cobl = child[5].text, craion = child[6].text, cgorod = child[7].text, cadres = child[8].text,
