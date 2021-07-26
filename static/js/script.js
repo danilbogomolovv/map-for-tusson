e681568d2177546fe15d487d229f8f2fe1aa2f7a
danilbogomolov <danilbogomolov.1710@gmail.com>
2021-07-26 19:01:11 +0300

    fixed disappearance of form after update

--- static/js/script.js
+++ static/js/script.js
@@ -4,11 +4,18 @@ checkboxes = document.getElementsByName('zones');
 
   for(var i=0, n=checkboxes.length;i<n;i++) {
     for (j in localStorage) {
+      console.log(localStorage[j])
       if (j == checkboxes[i].id && localStorage[j] == 'true') {
         document.getElementById(j).setAttribute('checked', localStorage[j])
       }
     } 
   }
+
+  document.getElementById('repair_form').style.display = localStorage.getItem('repair_form')
+  document.getElementById('repair_tids').style.display = localStorage.getItem('repair_tids')
+  document.getElementById('terminals_for_repair').style.width = localStorage.getItem('terminals_for_repair_width')
+  document.getElementById('repair_form_make_button').style.display = localStorage.getItem('repair_form_make_button')
+  document.getElementById('repair_form_clear_button').style.display = localStorage.getItem('repair_form_clear_button')
 }
 
 
@@ -30,14 +37,25 @@ function ShowFormCloseTids(){
   document.getElementById('terminals_for_repair').style.width = '24%'
   document.getElementById('repair_form_make_button').style.display = 'block'
   document.getElementById('repair_form_clear_button').style.display = 'block'
+  localStorage.setItem('repair_tids', 'none') 
+  localStorage.setItem('repair_form', 'block')
+  localStorage.setItem('terminals_for_repair_width', '24%') 
+  localStorage.setItem('repair_form_make_button', 'block')
+  localStorage.setItem('repair_form_clear_button', 'block')
+
 }
 
 function ShowTidsCloseForm(){
   document.getElementById('repair_tids').style.display = 'block'
   document.getElementById('repair_form').style.display = 'none'
   document.getElementById('terminals_for_repair').style.width = '16%'
-    document.getElementById('repair_form_make_button').style.display = 'none'
+  document.getElementById('repair_form_make_button').style.display = 'none'
   document.getElementById('repair_form_clear_button').style.display = 'none'
+  localStorage.setItem('repair_tids', 'block') 
+  localStorage.setItem('repair_form', 'none')
+  localStorage.setItem('terminals_for_repair_width', '16%') 
+  localStorage.setItem('repair_form_make_button', 'none')
+  localStorage.setItem('repair_form_clear_button', 'none')
 }
 
 function ClearRepairInfo() {
